ec6b9d8ff0964a9c300ec6cdd2f188b0c1912fd8
Bernard Quizon <quizonjrbernard@gmail.com>
2026-05-22 13:43:48 +0400

    BQ - feat: initialize special-stage-cafe application with Angular SSR setup
    - Added server-side rendering configuration in app.config.server.ts
    - Created application configuration for browser in app.config.ts
    - Implemented main entry points for server and browser in main.ts and main.server.ts
    - Defined routing for server and client in app.routes.ts and app.routes.server.ts
    - Developed core application structure including App component, Navbar, Footer, and feature components (Home, Menu, Login, Register)
    - Added styles and HTML templates for components
    - Set up testing for components with spec files
    - Configured TypeScript settings with tsconfig files
    - Created server setup with Express and Angular Node App Engine

---
+++ special-stage-cafe/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './features/home/home';
+import { MenuComponent } from './features/menu/menu';
+import { LoginComponent } from './features/auth/login/login';
+import { RegisterComponent } from './features/auth/register/register';
+
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
